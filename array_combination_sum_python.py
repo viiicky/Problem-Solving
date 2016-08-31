f6f50bcc63d2b921d712f5b56616ec8c43aeaa9c
Vikas Prasad <vikasprasad.prasad@gmail.com>
2016-08-31 16:37:47 +0530

    Add files via upload

--- array_combination_sum_python.py
+++ array_combination_sum_python.py
@@ -1,14 +1,18 @@
+'''Return all possible subset from the given collection 'numbers'
+    whose sum results in 'total'.
+'''
+
 from itertools import combinations
 
 numbers = [1, 1, 1, 2, 3, 5, 7]
 total = 8
 
-required_combination = []
+required_combination = set()
 for i in range(1, len(numbers)+1):
     combination_list = list((combinations(numbers, i)))
     
     for comb in combination_list:
         if sum(comb) == total:
-            required_combination.append(comb)
+            required_combination.add(comb)
 
-print(set(required_combination))
+print(required_combination)

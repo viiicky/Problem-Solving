5091a0efa17269f03ef4d196956df4b4c898177f
viiicky <vikasprasad.prasad@gmail.com>
2021-04-05 00:49:13 +0530

    Add LeetCode solution #1337

---
+++ LeetCode/1337.The_K_Weakest_Rows_in_a_Matrix.py
@@ -0,0 +1,46 @@
+from typing import List
+import heapq
+
+
+def heappush(heap, item):
+    return heapq.heappush(heap, -item)
+
+
+def heappushpop(heap, item):
+    return heapq.heappushpop(heap, -item)
+
+
+def heappop(heap):
+    return heapq.heappop(heap)
+
+
+class Row:
+    def __init__(self, value, pos):
+        self.value = value
+        self.pos = pos
+
+    def __lt__(self, other):
+        return self.pos < other.pos if self.value == other.value else self.value < other.value
+
+    def __neg__(self):
+        return Row(-self.value, -self.pos)
+
+    def __repr__(self):
+        return f'value: {self.value}, pos: {self.pos}'
+
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        heap = []
+        for i in range(k):
+            row = Row(sum(mat[i]), i)
+            heappush(heap, row)
+            print(heap)
+
+        for row_pos, row_value in enumerate(mat[k:], start=k):
+            value = Row(sum(row_value), row_pos)
+            if value < -heap[0]:
+                heappushpop(heap, value)
+            print(heap)
+
+        return [(-heappop(heap)).pos for _ in range(k)][::-1]

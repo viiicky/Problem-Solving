86ce1149350c9af86809768e6ceadfa54aad4811
viiicky <vikasprasad.prasad@gmail.com>
2021-04-08 10:04:03 +0530

    Add new solutions for #1337

--- LeetCode/1337.The_K_Weakest_Rows_in_a_Matrix.py
+++ LeetCode/1337.The_K_Weakest_Rows_in_a_Matrix.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 import heapq
 
@@ -11,7 +12,11 @@ def heappushpop(heap, item):
 
 
 def heappop(heap):
-    return heapq.heappop(heap)
+    return -heapq.heappop(heap)
+
+
+def heapmax(heap):
+    return -heap[0]
 
 
 class Row:
@@ -39,7 +44,73 @@ class Solution:
 
         for row_pos, row_value in enumerate(mat[k:], start=k):
             value = Row(sum(row_value), row_pos)
-            if value < -heap[0]:
+            if value < heapmax(heap):
                 heappushpop(heap, value)
 
-        return [(-heappop(heap)).pos for _ in range(k)][::-1]
+        return [(heappop(heap)).pos for _ in range(k)][::-1]
+
+
+class Solution2:
+    # Time complexity: O(m logn + m logm)
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        heap = []
+        for i in range(k):
+            row = Row(count_ones(mat[i]), i)
+            heappush(heap, row)
+
+        for row_pos, row_value in enumerate(mat[k:], start=k):
+            value = Row(count_ones(row_value), row_pos)
+            if value < heapmax(heap):
+                heappushpop(heap, value)
+
+        return [(heappop(heap)).pos for _ in range(k)][::-1]
+
+
+class Solution3:
+    # Time complexity: O(mn + m logm) amortised
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        counts = defaultdict(list)
+        for pos, row in enumerate(mat):
+            count = sum(row)
+            counts[count].append(pos)
+
+        output = []
+        for key in sorted(counts):
+            values = counts[key]
+            for value in values:
+                output.append(value)
+                if len(output) == k:
+                    return output
+
+
+class Solution4:
+    # Time complexity: O(m logn + m logm) amortised
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        counts = defaultdict(list)
+        for pos, row in enumerate(mat):
+            count = count_ones(row)
+            counts[count].append(pos)
+
+        output = []
+        for key in sorted(counts):
+            values = counts[key]
+            for value in values:
+                output.append(value)
+                if len(output) == k:
+                    return output
+
+
+def count_ones(items):
+    start = real_start = 0
+    end = real_end = len(items) - 1
+
+    while True:
+        mid = (start + end) // 2
+        if (items[mid] == 1 and mid == real_end) or (items[mid] == 1 and items[mid + 1] == 0):
+            return mid + 1
+        elif items[mid] == 1:
+            start = mid + 1
+        elif items[mid] == 0 and mid == real_start:
+            return mid
+        elif items[mid] == 0:
+            end = mid - 1

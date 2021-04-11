9fc40d90dab914014fd2529178dcde985b21ae8e
viiicky <vikasprasad.prasad@gmail.com>
2021-04-11 17:47:34 +0530

    Add LeetCode solution #933

---
+++ LeetCode/933.Number_of_Recent_Calls.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+
+class RecentCounter:
+    def __init__(self):
+        self.requests = []
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        while self.requests[0] < t - 3000:
+            self.requests.pop(0)
+
+        return len(self.requests)
+
+
+class RecentCounter2:
+    def __init__(self):
+        self.requests = deque()
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        while self.requests[0] < t - 3000:
+            self.requests.popleft()
+
+        return len(self.requests)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
